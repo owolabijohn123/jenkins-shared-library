0e6c1ae595b25dd3e283fcde94adee9a2f3702c9
yinkaowolabi091-web <yinkaowolabi091@gmail.com>
2026-02-23 17:35:37 +0100

    Update trivy_scan.groovy

--- vars/trivy_scan.groovy
+++ vars/trivy_scan.groovy
@@ -1,6 +1,10 @@
-
 def call() {
     sh """
-        trivy image ${env.DOCKER_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
+    docker run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        aquasec/trivy:latest image ${env.DOCKER_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
+    docker run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        aquasec/trivy:latest image ${env.DOCKER_MIGRATION_IMAGE_NAME}:${env.DOCKER_IMAGE_TAG}
     """
 }
