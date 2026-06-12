235eff6891396342494642a2ec5821e8edd6388d
owolabijohn123 <98545727+owolabijohn123@users.noreply.github.com>
2026-06-12 16:19:23 +0100

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,10 +1,13 @@
-// vars/run_tests.groovy
-def call() {
-    sh '''
-    docker run --rm \
-        -v "$(pwd)":/app \
-        -w /app \
-        node:18 \
-        sh -c "npm install && (npm run test || echo 'No tests defined, skipping...')"
-    '''
+def call(Map config) {
+    withCredentials([usernamePassword(
+        credentialsId: config.credentials,
+        usernameVariable: 'DOCKER_USER',
+        passwordVariable: 'DOCKER_PASS'
+    )]) {
+
+        sh """
+            echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+            docker push ${config.imageName}:${config.imageTag}
+        """
+    }
 }
