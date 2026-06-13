e7793f25bdd032def2bab686f66c92d697caf9fd
owolabijohn123 <98545727+owolabijohn123@users.noreply.github.com>
2026-06-13 16:55:53 +0100

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -1,16 +1,13 @@
 def call(Map config) {
-
     // Update deployment YAML files
     sh """
         # Update main app image
         sed -i "s|image: .*easyshop-app.*|image: dreedsir12/easyshop-app:${config.imageTag}|" ${config.manifestsPath}/08-easyshop-deployment.yaml
-
         # Update migration job image (if exists)
         if [ -f ${config.manifestsPath}/12-migration-job.yaml ]; then
             sed -i "s|image: .*easyshop-migration.*|image: dreedsir12/easyshop-migration:${config.imageTag}|" ${config.manifestsPath}/12-migration-job.yaml
         fi
     """
-
     // Commit & Push changes securely
     withCredentials([usernamePassword(
         credentialsId: config.gitCredentials,
@@ -20,11 +17,9 @@ def call(Map config) {
         sh """
             git config user.name "${config.gitUserName}"
             git config user.email "${config.gitUserEmail}"
-
             git add ${config.manifestsPath}/*.yaml
             git commit -m "Update image tag to ${config.imageTag}" || echo "No changes to commit"
-
-            git push https://${GIT_USER}:${GIT_PASS}@https://github.com/owolabijohn123/ecommerce-project.git HEAD:master
+            git push https://${GIT_USER}:${GIT_PASS}@github.com/owolabijohn123/ecommerce-project.git HEAD:master
         """
     }
 }
